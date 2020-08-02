730ff27f950a3658f2294aefcccd9bb185d245f7
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-08-03 03:21:36 +0700

    UPDATE: random avatar on register user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -66,8 +66,23 @@ class UserController extends Controller
                 'message' => 'email sudah digunakan'
             ]);
         }
+        $vector_avatar = [
+            'https://semantic-ui.com/images/avatar2/large/elyse.png',
+            'https://semantic-ui.com/images/avatar2/large/matthew.png',
+            'https://semantic-ui.com/images/avatar2/large/kristy.png',
+            'https://semantic-ui.com/images/avatar2/large/molly.png',
+            'https://semantic-ui.com/images/avatar/large/elliot.jpg',
+            'https://semantic-ui.com/images/avatar/large/jenny.jpg',
+            'https://semantic-ui.com/images/avatar/large/steve.jpg',
+            'https://semantic-ui.com/images/avatar/large/daniel.jpg',
+            'https://semantic-ui.com/images/avatar/large/helen.jpg',
+            'https://semantic-ui.com/images/avatar/large/elliot.jpg',
+            'https://semantic-ui.com/images/avatar/large/stevie.jpg',
+            'https://semantic-ui.com/images/avatar/large/veronika.jpg'
+        ];
         $input = $request->all();
         $input['username'] = $request->username ? $request->username : $faker->name();
+        $input['avatar'] = $vector_avatar[rand(0, count($vector_avatar) - 1)];
         $input['password'] = bcrypt($request->password);
         $user = User::create($input);
         $success['user'] = $user;
